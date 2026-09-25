d7edc64587134768db443baea599f5675ef2dfda
dmukunzi <d.mukunzi@alustudent.com>
2026-09-25 03:26:16 -0700

    Add grep to sort critical records

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -4,6 +4,10 @@
  process_vitals()
  {
    mkdir -p reports
- }
+   {
+     grep "CRITICAL" active_logs/heart_rate_log.log
+     grep "CRITICAL" active_logs/temperature_log.log
+   }
+}
 
  process_vitals
