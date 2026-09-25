d689d5038ab3e0f87f912d397debfff13de79fca
dmukunzi <d.mukunzi@alustudent.com>
2026-09-25 03:33:18 -0700

    Add awk and generate the report

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -7,7 +7,7 @@
    {
      grep "CRITICAL" active_logs/heart_rate_log.log
      grep "CRITICAL" active_logs/temperature_log.log
-   }
+   } | awk -F '|' '{print $1 " | " $2 " | " $3}' > reports/critical_alerts.txt
 }
 
  process_vitals
